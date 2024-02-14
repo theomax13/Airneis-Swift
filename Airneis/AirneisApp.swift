d8f08cf0f5e4c8f9ad7dfb6575749ccbfbf9ab32
MAXIME Théo <theomax13@hotmail.fr>
2024-02-14 17:28:09 +0100

    init

---
+++ Airneis/AirneisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirneisApp.swift
+//  Airneis
+//
+//  Created by Théo Maxime on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirneisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airneis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Airneis
+//
+//  Created by Théo Maxime on 14/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
